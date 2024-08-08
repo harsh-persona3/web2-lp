6f9f9b35e43b4d776d73b174a562af83d08a5f22
Vineet Sawhney <vineet@persona3.io>
2024-08-08 14:26:40 +0530

    fix: css on payment page

--- src/app/demo/payment/page.tsx
+++ src/app/demo/payment/page.tsx
@@ -6,7 +6,7 @@ const PaymentComponent = () => {
   return (
     <div className="flex min-h-screen  w-full justify-center items-center bg-white">
       <div className="size-full bg-white rounded-lg max-w-screen-2xl">
-        <div className="flex flex-col md:flex-row size-full h-screen">
+        <div className="flex flex-col md:flex-row size-full min-h-screen">
           <Suspense fallback="Loading..">
             <Breakup />
           </Suspense>
